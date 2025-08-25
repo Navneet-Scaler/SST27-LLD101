9848acf471ac4ff0a07f8a629308a8c8c82a0077
Navneet-Scaler <navneet.23bcs10136@ms.sst.scaler.com>
2025-08-25 11:32:21 +0530

    ex01 SOLID Violation Fixed

--- SOLID/ex01/src/Demo01.java
+++ SOLID/ex01/src/Demo01.java
@@ -2,6 +2,8 @@
 
 public class Demo01 {
     public static void main(String[] args) {
-        new OrderService().checkout("a@shop.com", 100.0);
+        EmailClient email = new EmailClient();
+        OrderService orderService = new OrderService(email);
+        orderService.checkout("a@shop.com", 100.0);
     }
 }
--- SOLID/ex01/src/OrderService.java
+++ SOLID/ex01/src/OrderService.java
@@ -1,13 +1,20 @@
 public class OrderService {
     double taxRate = 0.18;
-    EmailClient email = new EmailClient();
+    EmailClient emailClient;
+
+    // EmailClient email = new EmailClient(); // Tightly coupled earlier
+
+    public OrderService(EmailClient emailClient) {
+        this.emailClient = emailClient;
+    }
 
     double totalWithTax(double subtotal) {
         return subtotal + subtotal * taxRate;
     }
+
     void checkout(String customerEmail, double subtotal) {
         double total = totalWithTax(subtotal);
-        email.send(customerEmail, "Thanks! Your total is " + total);
         System.out.println("Order stored (pretend DB).");
+        emailClient.send(customerEmail, "Thanks! Your total is " + total);
     }
 }
